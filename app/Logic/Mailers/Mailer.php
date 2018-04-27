578ab958928ae241557f87a6040662eeec6a699f
ScrambledRK <ScrambledRK@gmail.com>
2018-04-27 11:17:12 +0200

    #75 added text only version in addition to html mails

--- app/Logic/Mailers/Mailer.php
+++ app/Logic/Mailers/Mailer.php
@@ -2,27 +2,31 @@
 
 namespace App\Logic\Mailers;
 
-abstract class Mailer {
+abstract class Mailer
+{
     protected $beautymail;
 
-    public function __construct(){
-      $this->beautymail = app()->make(\Snowfire\Beautymail\Beautymail::class);
-    }
-
-    public function sendTo($email, $name = '', $subject, $fromEmail, $view, $data = [])
+    public function __construct()
     {
-      //  dd($email, $name, $subject, $fromEmail, $view, $data);
-
-
-      $this->beautymail->queue($view, $data, function($message) use ($email, $name, $subject, $fromEmail)
-      {
-          $message
-              ->to($email,$name)    // do not set "from" and spoof a sender, this will fail on the mail-server
-              ->subject($subject);
-      });
+        $this->beautymail = app()->make( \Snowfire\Beautymail\Beautymail::class );
     }
-    public function view($view, $data){
-      return $this->beautymail->view($view,$data);
+
+    public function sendTo( $email, $name = '', $subject, $fromEmail, $view, $data = [] )
+    {
+        //  dd($email, $name, $subject, $fromEmail, $view, $data);
+
+
+        $this->beautymail->queue( $view, $data, function( $message ) use ( $email, $name, $subject, $fromEmail )
+        {
+            $message
+                ->to( $email, $name )// do not set "from" and spoof a sender, this will fail on the mail-server
+                ->subject( $subject );
+        } );
+    }
+
+    public function view( $view, $data )
+    {
+        return $this->beautymail->view( $view, $data );
     }
 
 }
--- app/Logic/Mailers/UserMailer.php
+++ app/Logic/Mailers/UserMailer.php
@@ -1,20 +1,25 @@
 <?php
-namespace App\Logic\Mailers;
-class UserMailer extends Mailer {
-    public function verify(\App\User $user)
-    {
-        $view       = 'email.verify';
-        $subject    = 'Verify my email';
-        $fromEmail  = env('FROM_MAIL');
 
-        return $this->sendTo($user->email, $user->name, $subject, $fromEmail, $view, ['confirmation_code' => $user->confirmation_code]);
-    }
-    public function passwordReset(\App\User $user, $data)
+namespace App\Logic\Mailers;
+
+//
+class UserMailer extends Mailer
+{
+    public function verify( \App\User $user )
     {
-        $view       = 'email.password-reset';
-        $subject    = 'Reset your password';
-        $fromEmail  = env('FROM_MAIL');
-        $this->sendTo($user->email, $user->name, $subject, $fromEmail, $view, $data);
+        $view = ['email.verify','email.verify-text'];
+        $subject = 'Verify my email';
+        $fromEmail = env( 'FROM_MAIL' );
+
+        return $this->sendTo( $user->email, $user->name, $subject, $fromEmail, $view, [ 'confirmation_code' => $user->confirmation_code ] );
+    }
+
+    public function passwordReset( \App\User $user, $data )
+    {
+        $view = ['email.password-reset','email.password-reset-text'];
+        $subject = 'Reset your password';
+        $fromEmail = env( 'FROM_MAIL' );
+        $this->sendTo( $user->email, $user->name, $subject, $fromEmail, $view, $data );
     }
 
 }
