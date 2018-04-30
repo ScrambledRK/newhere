bc7215e5d724b4938d41745c46dd746a135ae886
ScrambledRK <ScrambledRK@gmail.com>
2018-04-30 10:47:15 +0200

    removed ngo/offer seeds

--- database/seeds/NgoSeeder.php
+++ database/seeds/NgoSeeder.php
@@ -18,30 +18,31 @@ class NgoSeeder extends Seeder
         //
 
 
-        $reader = Reader::createFromPath(base_path().'/database/seeds/csvs/ngos.csv');
-        $reader->setDelimiter(";");
-        $results = $reader->fetch();
-        foreach ($results as $key => $row) {
-
-            $ngo = new Ngo();
-            $ngo->short = $row[0];
-            $ngo->organisation = $row[1];
-            $ngo->contact_email = $row[2];
-            $ngo->website = $row[4];
-            $ngo->contact = $row[5];
-            $ngo->contact_phone = $row[6];
-            $ngo->street = $row[7];
-            $ngo->street_number = $row[8];
-            $ngo->zip = $row[9];
-            $ngo->city = $row[10];
-            $ngo->published = true;
-            $ngo->save();
-
-            $ngo->translateOrNew('de')->description = $row[12];
-
-            $ngo->save();
-
-        }
+//        $reader = Reader::createFromPath(base_path().'/database/seeds/csvs/ngos.csv');
+//        $reader->setDelimiter(";");
+//        $results = $reader->fetch();
+//
+//        foreach ($results as $key => $row) {
+//
+//            $ngo = new Ngo();
+//            $ngo->short = $row[0];
+//            $ngo->organisation = $row[1];
+//            $ngo->contact_email = $row[2];
+//            $ngo->website = $row[4];
+//            $ngo->contact = $row[5];
+//            $ngo->contact_phone = $row[6];
+//            $ngo->street = $row[7];
+//            $ngo->street_number = $row[8];
+//            $ngo->zip = $row[9];
+//            $ngo->city = $row[10];
+//            $ngo->published = true;
+//            $ngo->save();
+//
+//            $ngo->translateOrNew('de')->description = $row[12];
+//
+//            $ngo->save();
+//
+//        }
 
         $ngo = new Ngo();
         $ngo->short = 'NONE';
